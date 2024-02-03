59a4748cb41923fe6fed7ee21d72f704130f8e20
Jess <158598543+Jessweiss06@users.noreply.github.com>
2024-02-03 12:07:07 +0000

    Update main.py
    updated main to work with the new terminal

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 
 clock = pygame.time.Clock()
 game = cl.game()
+screenWidth = 0
 fps = 60
 
 print("\n\n\nRunning...")
@@ -10,17 +11,25 @@ print("\n\n\nRunning...")
 # Girl init
 byoeimero = cl.girl(game)
 byoeimero.imageLocation = "Byoeimero_Yuokini\\NHK_Concept.png"
-byoeimero.imageResolution = (413, 584)
+byoeimero.imageResolution = (1690, 3508)
+byoeimero.imageScaler = 1/6
 byoeimero.name = "Byoeimero"
 byoeimero.dialogueArray = [
-("Hello Tatsuhiro UWU", 0)
+("Hello Tatsuhiro", 0),
+("I'm Byoeimero UWU", 0)
 ]
-
 
 while game.running:
 
-	# Girl Test
-	byoeimero.draw(byoeimero.dialogueArray[byoeimero.pointer][0])
+	if game.screen.get_size()[0] != screenWidth:
+		# Girl Test
+		green = (0, 150, 0)
+		byoeimero.drawFrame()
+		game.terminalDraw()
+		game.terminalWrite(byoeimero.dialogueArray[byoeimero.pointer][0], "Monokai", 20, green)
+		byoeimero.pointer += 1
+		game.terminalWrite(byoeimero.dialogueArray[byoeimero.pointer][0], "Monokai", 20, green)
+		screenWidth = game.screen.get_size()[0]
 
 	events = pygame.event.get()
 	for event in events:
