6471027c464b6d82ddc57631b82a80482c95359a
Jess <158598543+Jessweiss06@users.noreply.github.com>
2024-02-09 13:57:16 +0000

    Update script.py
    changed advance method to be more versatile

--- script.py
+++ script.py
@@ -4,22 +4,21 @@ from Rei import rei
 
 class script:
 
-	def __init__(self, game):
+	def __init__(self, game, terminal):
 
-		self.byoeimero = byoeimero.init(game, cl.girl)
-		self.rei = rei.init(game, cl.girl)
+		self.byoeimero = byoeimero.init(game, terminal, cl.girl)
+		self.rei = rei.init(game, terminal, cl.girl)
 
-		game.terminalCache = []
+		terminal.cache = []
 
 		self.pointer = 0
-		self.script = [
-		(self.byoeimero, cl.game.format2),
-		(self.rei, cl.game.format3)
-		]
+		self.halt = False
+		self.script = open("scriptCode.py", "r").readlines()
 
 	def advance(self, game):
 
-		self.script[self.pointer][0].advance(self.script[self.pointer][1])
+		exec(self.script[self.pointer])
+		if self.halt: return
 		self.pointer += 1
 
 	def update(self):
