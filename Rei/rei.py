abef95b07143bc24ee7ac6c786429ef464254b7d
Jess <158598543+Jessweiss06@users.noreply.github.com>
2024-02-13 14:07:42 +0000

    Update rei.py
    changed rei to work with updated drawFrame method

--- Rei/rei.py
+++ Rei/rei.py
@@ -1,3 +1,6 @@
+# In dialogue Arrays:
+# 0 means the dialogue is finished
+# 1 means the dialogue continues from the previous index
 
 dialogueArray = [
 ("Hey Tatsuhiro!", 0),
@@ -6,15 +9,18 @@ dialogueArray = [
 ("", 0)
 ]
 
+imageLocation = [
+("Rei\\NHK_Concept.png", (817, 3508), 1/6)
+]
+
 # init requires an instance of the game class, initialised in main
+# and an instance of the terminal class, initialised in main
 # and the girl class (not an instance)
 
 def init(game, terminal, girl):
 	rei = girl(game, terminal)
-	rei.imageLocation = "Rei\\NHK_Concept.png"
-	rei.imageResolution = (817, 3508)
-	rei.imageScaler = 1/6
-	rei.name = "Rei"
 	rei.dialogueArray = dialogueArray
+	rei.imageLocation = imageLocation
+	rei.name = "Rei"
 
 	return rei
