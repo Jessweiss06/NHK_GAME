f59d1a072fc5b464e405c79bfb9c384268f9706e
Jess <158598543+Jessweiss06@users.noreply.github.com>
2024-02-13 15:39:01 +0000

    Update scriptCode.py
    made use of the previous change to classes

--- scriptCode.py
+++ scriptCode.py
@@ -1,16 +1,13 @@
 self.currentGirl = self.misaki
-self.misaki.currentFrame = 0
-self.misaki.drawFrame(self.misaki.currentFrame)
+self.misaki.drawFrame(0)
 self.misaki.advance(cl.game.format2)
 self.halt = True
 
 self.currentGirl = self.byoeimero
-self.byoeimero.currentFrame = 0
-self.byoeimero.drawFrame(self.byoeimero.currentFrame)
-self.byoeimero.advance(cl.game.format2)
+self.byoeimero.drawFrame(0)
+self.byoeimero.advance(cl.game.format4)
 self.halt = True
 
 self.currentGirl = self.rei
-self.rei.currentFrame = 0
-self.rei.drawFrame(self.rei.currentFrame)
+self.rei.drawFrame(0)
 self.rei.advance(cl.game.format3)
