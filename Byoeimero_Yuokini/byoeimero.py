92de7e7669c31a9c7cb4b3f58b917ae85e1b4a61
Jess <158598543+Jessweiss06@users.noreply.github.com>
2024-02-13 14:06:36 +0000

    Update byoeimero.py
    changed byoeimero to work with drawFrame update

--- Byoeimero_Yuokini/byoeimero.py
+++ Byoeimero_Yuokini/byoeimero.py
@@ -1,3 +1,6 @@
+# In dialogue Arrays:
+# 0 means the dialogue is finished
+# 1 means the dialogue continues from the previous index
 
 dialogueArray = [
 ("Hello Tatsuhiro", 0),
@@ -13,15 +16,18 @@ dialogueArray = [
 ("", 0),
 ]
 
+imageLocation = [
+("Byoeimero_Yuokini\\NHK_Concept.png", (2480, 3508), 1/6)
+]
+
 # init requires an instance of the game class, initialised in main
+# and an instance of the terminal class, initialised in main
 # and the girl class (not an instance)
 
 def init(game, terminal, girl):
 	byoeimero = girl(game, terminal)
-	byoeimero.imageLocation = "Byoeimero_Yuokini\\NHK_Concept.png"
-	byoeimero.imageResolution = (2480, 3508)
-	byoeimero.imageScaler = 1/6
-	byoeimero.name = "Byoeimero"
 	byoeimero.dialogueArray = dialogueArray
+	byoeimero.imageLocation = imageLocation
+	byoeimero.name = "Byoeimero"
 
 	return byoeimero
