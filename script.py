2499c962a144e09e765fd6edb0bfb2e8d76bdee6
Jess <158598543+Jessweiss06@users.noreply.github.com>
2024-02-13 16:53:21 +0000

    Update script.py
    added rudimentary developer commands

--- script.py
+++ script.py
@@ -24,3 +24,34 @@ class script:
 		exec(self.script[self.pointer])
 		if self.halt: return
 		self.pointer += 1
+
+	# run will be used for any developer commands
+
+	def run(self, command):
+
+		term = command.split()
+		if len(term) < 2: return
+
+		classs = {
+		"rei": self.rei,
+		"misaki": self.misaki,
+		"byoeimero": self.byoeimero
+		}
+
+		method = {
+		"appraise": self.appraise(classs[term[1]])
+		}
+
+		if term[1] in classs and term[0] in method:
+			method[term[0]]
+
+	# <Developer Commands>
+	def appraise(self, girl):
+
+		name = girl.name
+		affection = girl.affection
+		finalS = "s" if affection != 1 else ""
+
+		apraisal = f"{name} has {affection} affection point{finalS}"
+		girl.terminal.cache.append((apraisal, cl.game.format5))
+	# </Developer Commands>
