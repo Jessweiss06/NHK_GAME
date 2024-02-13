16f3c4e57df352ed69bbe4f78104053b6360f59d
Jess <158598543+Jessweiss06@users.noreply.github.com>
2024-02-13 14:09:12 +0000

    Update scriptCode.py
    changed scriptCode to work with updated drawFrame method

--- scriptCode.py
+++ scriptCode.py
@@ -1,13 +1,16 @@
 self.currentGirl = self.misaki
-self.misaki.drawFrame()
+self.misaki.currentFrame = 0
+self.misaki.drawFrame(self.misaki.currentFrame)
 self.misaki.advance(cl.game.format2)
 self.halt = True
 
 self.currentGirl = self.byoeimero
-self.byoeimero.drawFrame()
+self.byoeimero.currentFrame = 0
+self.byoeimero.drawFrame(self.byoeimero.currentFrame)
 self.byoeimero.advance(cl.game.format2)
 self.halt = True
 
 self.currentGirl = self.rei
-self.rei.drawFrame()
+self.rei.currentFrame = 0
+self.rei.drawFrame(self.rei.currentFrame)
 self.rei.advance(cl.game.format3)
