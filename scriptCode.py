a616d9bd30e10446902a10f3aa27bd67df897a68
Jess <158598543+Jessweiss06@users.noreply.github.com>
2024-02-13 12:52:16 +0000

    Update scriptCode.py
    script can now draw characters

--- scriptCode.py
+++ scriptCode.py
@@ -1,5 +1,13 @@
+self.currentGirl = self.misaki
+self.misaki.drawFrame()
 self.misaki.advance(cl.game.format2)
 self.halt = True
+
+self.currentGirl = self.byoeimero
+self.byoeimero.drawFrame()
 self.byoeimero.advance(cl.game.format2)
 self.halt = True
+
+self.currentGirl = self.rei
+self.rei.drawFrame()
 self.rei.advance(cl.game.format3)
