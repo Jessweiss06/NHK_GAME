3784a2882b95bfb29f9248db5412081276c5b3c0
Jess <158598543+Jessweiss06@users.noreply.github.com>
2024-02-13 12:49:38 +0000

    Update misaki.py
    fixed dialogueArray formatting
    and added clearer instructions on how to use them

--- Misaki/misaki.py
+++ Misaki/misaki.py
@@ -1,12 +1,16 @@
+# In dialogue Arrays:
+# 0 means the dialogue is finished
+# 1 means the dialogue continues from the previous index
 
 dialogueArray = [
 ("Hey! You're one hour late.", 0),
 ("Did you bring the contract?", 1),
-("What does object-oriented programming mean?", 2),
+("What does object-oriented programming mean?", 0),
 ("", 0)
 ]
 
 # init requires an instance of the game class, initialised in main
+# and an instance of the terminal class, initialised in main
 # and the girl class (not an instance)
 
 def init(game, terminal, girl):
